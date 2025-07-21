f261fb021fb84aecd30e2cb7f4e5bb96ae6cf191
thneves <thales.neves@pinpeople.com.br>
2025-07-20 21:56:32 -0300

    refactor: move catfile to new class, fix mode bug, change method name

---
+++ lib/cat_file.rb
@@ -0,0 +1,43 @@
+class CatFile
+  MYGIT_DIR = '.mygit'.freeze
+  OBJECTS_DIR = "#{MYGIT_DIR}/objects".freeze
+
+  def self.call(hash, mode)
+    new.call(hash, mode)
+  end
+
+  def call(hash, mode)
+    decompressed_file = decompress_file(hash)
+    return decompressed_file[:content] if mode == 'content'
+
+    decompressed_file[:type].split.first
+  end
+
+  private
+
+  def decompress_file(hash)
+    location = object_location(hash)
+
+    compressed_file = File.binread("#{location[:dir]}/#{location[:filename]}")
+
+    decompressed_file = Zlib::Inflate.inflate(compressed_file)
+
+    type, content = decompressed_file.split("\0", 2)
+    
+    {
+      type:,
+      content:
+    }
+  end
+
+  def object_location(hash)
+    hash_dir_name = hash.slice 0..1
+    dir = "#{OBJECTS_DIR}/#{hash_dir_name}"
+    filename = hash.slice 2..-1
+
+    {
+      dir:,
+      filename:
+    }
+  end
+end
