4d559f69ebafec95e44fd53750b8de5032885e35
thneves <thales.neves@pinpeople.com.br>
2025-07-27 11:35:04 -0300

    fix: checks all files inside index file

--- lib/staging.rb
+++ lib/staging.rb
@@ -7,8 +7,14 @@ class Staging
   include Constants
   include Commons
   
+  ALL_FILES = '.'.freeze
+
   def self.add(files)
     new.add(files)
+  end
+
+  def self.status
+    new.status
   end
 
   def add(files)
@@ -16,7 +22,7 @@ class Staging
     File.write(INDEX_FILE, '') if !File.exist?(INDEX_FILE) 
     
     modified_files = []
-
+    
     files.each do |file|
       indexed = File.read(INDEX_FILE).include?(file)
       if indexed
@@ -38,10 +44,70 @@ class Staging
     
     # writing files to index
     modified_files.each do |file|
-      byebug
       new_entry = new_index_entry(file)
       File.write(INDEX_FILE, new_entry, mode: 'a+') #append mode
     end
+  end
+
+  def status
+    # compare index vs HEAD -> detect staged changes -> changes to be committed
+    # compare working Dir vs index -> unstaged changes -> changes not staged to the commit
+    # compare working Dir vs index -> new, untracked files -> untracked files
+  
+    indexed_files = File.readlines(INDEX_FILE) if index?
+    index_list = {}
+    
+    indexed_files.each do |file_info|
+      file_info.strip! #["100644", "hello.txt", "0142824230f3fafbcb268341063276ed745b142e"]
+      mode     = file_info.split[0]
+      filename = file_info.split[1]
+      sha = file_info.split[2]
+
+      index_list[filename] = {
+        mode: mode,
+        sha: sha
+      }
+    end
+
+    latest_commit_tree = File.read(HEAD_FILE)
+    ref_path = latest_commit_tree.strip.split.last # refs/heads/main
+    last_commit_sha1 = File.read("#{MYGIT_DIR}/#{ref_path}") # "98bc26ea437c926bfc14473b0661f26bf9e6f5a6"
+
+    dir = last_commit_sha1[0..1] # 98
+    filename = last_commit_sha1[2..] # bc26ea437c926bfc14473b0661f26bf9e6f5a6
+
+    file_path = "#{OBJECTS_DIR}/#{dir}/#{filename}" # ".mygit/objects/98/bc26ea437c926bfc14473b0661f26bf9e6f5a6"
+
+    file = File.read(file_path) # BINARY FILE "x\x9C\x95\x8DK\u000E\xC20 ....
+
+    file = sha_file_finder(last_commit_sha1, binary: true) # BINARY FILE "x\x9C\x95\x8DK\u000E\xC20 ....
+    
+    decompressed_commit_file = Zlib::Inflate.inflate(file) # "commit 153\x00tree bc4a9474db732ffa3e5b6d47c8a5e924a02cc202\nauthor thales thales@iamgit.com 1753237221 +0000\ncommitter thales thales@iamgit.com 1753237221 +0000\noioioi"
+
+    commit_tree_sha = decompressed_commit_file.split[3]  # bc4a9474db732ffa3e5b6d47c8a5e924a02cc202
+
+    commit_tree_file = sha_file_finder(commit_tree_sha, binary: true) # BINARY FILE from commit 
+
+    decompressed_tree_file = Zlib::Inflate.inflate(commit_tree_file) # "tree 312\x00100644 .rspec\x00\xC9\x9D.s\x96\xE1J\xC0r\xC6>\xC8A\x9D\x9B\x8F\xED\xE2\x8D\x8640000 ...
+
+    byebug
+    head_tree = {
+
+    }
+    
+    puts "inprogrss"
+  end
+
+  def sha_file_finder(sha, binary: false)
+    dir = sha[0..1]
+    filename = sha[2..]
+    path = "#{OBJECTS_DIR}/#{dir}/#{filename}"
+    return File.binread(path) if binary
+    File.read(path)
+  end
+
+  def index?
+    File.exist? INDEX_FILE
   end
 
   private
